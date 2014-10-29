53a1fa8a6513b47d6e5ce5214cd4f5516c50e290
raphaelluchini <laster_raphael@hotmail.com>
2014-10-29 19:06:23 -0200

    using promises instead callbacks

--- cli.js
+++ cli.js
@@ -2,7 +2,7 @@ var samsung = require('./index');
 
 /**
 2 = Folder to be package
-3 = Folder that the server will run (Optional) Default:./pakages
+3 = Folder that the server will run (Optional) Default:./packages
 4 = App ID (Optional)
 5 = App Tile (Optional)
 6 = Port that the server will run (Optional) Default:80
