862679183accef889f67d1589ed03cae404a4434
raphaelluchini <laster_raphael@hotmail.com>
2014-10-29 15:09:02 -0200

    fixing cli params

--- cli.js
+++ cli.js
@@ -1,11 +1,11 @@
 var samsung = require('./index');
 
 /**
-1 = Folder to be package
-2 = Folder that the server will run (Optional) Default:./pakages
-3 = App ID (Optional)
-4 = App Tile (Optional)
-5 = Port that the server will run (Optional) Default:80
+2 = Folder to be package
+3 = Folder that the server will run (Optional) Default:./pakages
+4 = App ID (Optional)
+5 = App Tile (Optional)
+6 = Port that the server will run (Optional) Default:80
 **/
 
-samsung.run(process.argv[1], process.argv[2], process.argv[3], process.argv[4], process.argv[5]);
+samsung.run(process.argv[2], process.argv[3], process.argv[4], process.argv[5], process.argv[6]);
